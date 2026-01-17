4119d4c1fdffbba499c683c87df144d74cd4e146
Yadnyesh23 <yadnyeshhalde2006@gmail.com>
2026-01-17 19:39:34 +0530

    Intialized frontend and installed tailwind

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-4xl bg-black text-white text-center'>Authentication App</h1>
+    </>
+  )
+}
+
+export default App
